59f5d3fdcd315daa699c811700423ba76c5aa744
Emilin123 <clendifraga@gmail.com>
2026-09-18 12:02:10 -0500

    Update build.gradle.kts

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -1,7 +1,28 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
-android { namespace = "online.cee.app"; compileSdk = 35
-    defaultConfig { applicationId = "online.cee.app"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+android {
+    namespace = "online.cee.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "online.cee.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
